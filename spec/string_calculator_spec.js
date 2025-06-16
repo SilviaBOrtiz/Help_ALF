888e9fcc011b2882370e8a450fb0f3f473d748da
SilviaBOrtiz <silviariasortiz@gmail.com>
2025-06-16 18:20:09 +0100

    feat: find spaceship in position [0, 0]

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -1,13 +1,19 @@
 describe("Help ALF - findSpaceship", function () {
-  it("returns 'Spaceship lost forever.' if map is empty", function () {
-    expect(findSpaceship("")).toBe("Spaceship lost forever.");
-  });
+    it("returns 'Spaceship lost forever.' if map is empty", function () {
+        expect(findSpaceship("")).toBe("Spaceship lost forever.");
+    });
 
-  it("returns 'Spaceship lost forever.' if map contains no 'X'", function () {
-    const map = "...\n...\n...";
-     expect(findSpaceship(map)).toBe("Spaceship lost forever.");
-  });
+    it("returns 'Spaceship lost forever.' if map contains no 'X'", function () {
+        const map = "...\n...\n...";
+        expect(findSpaceship(map)).toBe("Spaceship lost forever.");
+    });
+
+    it("finds the spaceship at bottom-left corner [0, 0]", function () {
+        const map = "...\n...\nX..";
+        expect(findSpaceship(map)).toEqual([0, 0]);
+    });
 
 
+  
 
 });
