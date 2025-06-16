f159cc98a3889ded04ea71992d0dc5cf96237367
SilviaBOrtiz <silviariasortiz@gmail.com>
2025-06-16 17:05:46 +0100

    feat: find spaceship coordinates in 2D map

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -2,5 +2,14 @@
 function findSpaceship(map) {
   if (!map) return "Spaceship lost forever.";
 
+  const lines = map.split("\n").reverse(); //Y = 0 ist unten
+  for (let y = 0; y < lines.length; y++) {
+    const x = lines[y].indexOf("X");
+    if (x !== -1) {
+      return [x, y]; //Koordinaten zurückgeben
+    }
+  }
+
+  return "Spaceship lost forever."; //Kein 'X' gefunden
 }
 
